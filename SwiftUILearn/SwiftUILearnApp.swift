937eff8594fe4d526c482d850791b72b269a992d
Harlans <2461414445@qq.com>
2024-07-26 22:43:35 +0800

    init

---
+++ SwiftUILearn/SwiftUILearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearnApp.swift
+//  SwiftUILearn
+//
+//  Created by Harlans on 2024/7/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
