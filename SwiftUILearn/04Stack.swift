c29ad02a46bb68069176470013c5c81299eaaed2
Harlans <2461414445@qq.com>
2024-07-28 16:41:27 +0800

    增加课程 4、5

--- SwiftUILearn/04Stack.swift
+++ SwiftUILearn/04Stack.swift
@@ -9,10 +9,104 @@ import SwiftUI
 
 struct _Stack: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HeaderView()
+            _ContentView()
+            FooterView()
+            Spacer()
+        }
     }
 }
 
 #Preview {
     _Stack()
+}
+
+struct HeaderView: View {
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading, spacing: 2) {
+                Text("Choose")
+                    .font(.system(.largeTitle, design: .rounded))
+                    .fontWeight(.black)
+                Text("Your Plan")
+                    .font(.system(.largeTitle, design: .rounded))
+                    .fontWeight(.black)
+            }
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+struct PricingView: View {
+    var title: String
+    var price: String
+    var textColor: Color
+    var bgColor: Color
+    
+    var icon: String?
+    
+    
+    var body: some View {
+        VStack {
+            if let icon = icon {
+                Image(icon)
+                    .font(.largeTitle)
+                    .foregroundColor(textColor)
+            }
+            Text(title)
+                .font(.system(.title, design: .rounded))
+                .fontWeight(.black)
+                .foregroundColor(textColor)
+            Text(price)
+                .font(.system(size: 40, weight: .heavy, design: .rounded))
+                .foregroundColor(textColor)
+            Text("per month")
+                .font(.headline)
+                .foregroundColor(textColor)
+        }
+        .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity, minHeight: 100)
+        .padding(40)
+        .background(bgColor)
+        .cornerRadius(10)
+    }
+}
+
+struct _ContentView: View {
+    var body: some View {
+        HStack(spacing: 15) {
+            PricingView(title: "Basic", price: "$9", textColor: .white, bgColor: .purple)
+            ZStack {
+                PricingView(title: "Pro", price: "$19", textColor: .black, bgColor: Color(red: 240/255, green: 240/255, blue: 240/255))
+                Text("Best for designer")
+                    .font(.system(.caption, design: .rounded))
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .padding(5)
+                    .background(Color(red: 255/255, green: 183/255, blue: 37/255))
+                    .offset(x: 0, y: 84)
+                
+            }
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct FooterView: View {
+    var body: some View {
+        ZStack {
+            PricingView(title: "Team", price: "$299", textColor: .white, bgColor:  Color(red: 62/255, green: 63/255, blue: 70/255), icon: "wand.and.rays")
+                .padding()
+            
+            Text("Best for designer")
+                .font(.system(.caption, design: .rounded))
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(Color(red: 255/255, green: 183/255, blue: 37/255))
+                .offset(x: 0, y: 100)
+        }
+        
+    }
 }
--- SwiftUILearn/05ScrollView.swift
+++ SwiftUILearn/05ScrollView.swift
@@ -7,9 +7,100 @@
 
 import SwiftUI
 
+struct _51ScrollView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("monday, aug20".uppercased())
+                    Text("Your Reading")
+                        .font(.title)
+                        .fontWeight(.black)
+                }
+                Spacer()
+            }
+            .padding([.top, .horizontal])
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                
+                HStack {
+                    Group {
+                        CordView(image: "swiftui-button", category: "SwiftUI", heading: "Drawing a Border with Rounded Corners", author: "Simon Ng")
+                        CordView(image: "macos-programming", category: "macOS", heading: "Building a Simple Editing App", author: "Gabriel Theodoropoulos")
+                        CordView(image: "flutter-app", category: "Flutter", heading: "Building a Complex Layout with Flutter", author: "Lawrence Tan")
+                        CordView(image: "natural-language-api", category: "iOS", heading: "What's New in Natural Language API", author: "Sai Kambampati")
+                    }.frame(width: 300)
+                }
+                
+                Spacer()
+            }
+        }
+    }
+}
+
+#Preview {
+    _51ScrollView()
+}
+
+
 struct _5ScrollView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView() {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("monday, aug20".uppercased())
+                    Text("Your Reading")
+                        .font(.title)
+                        .fontWeight(.black)
+                }
+                Spacer()
+            }
+            .padding(.leading)
+            VStack {
+                CordView(image: "swiftui-button", category: "SwiftUI", heading: "Drawing a Border with Rounded Corners", author: "Simon Ng")
+                CordView(image: "macos-programming", category: "macOS", heading: "Building a Simple Editing App", author: "Gabriel Theodoropoulos")
+                CordView(image: "flutter-app", category: "Flutter", heading: "Building a Complex Layout with Flutter", author: "Lawrence Tan")
+                CordView(image: "natural-language-api", category: "iOS", heading: "What's New in Natural Language API", author: "Sai Kambampati")
+            }
+        }
+    }
+}
+
+struct CordView: View {
+    var image: String
+    var category: String
+    var heading: String
+    var author: String
+    
+    var body: some View {
+        VStack {
+            Image(image)
+                .resizable() // 修饰符使得图像可以根据其父视图的大小进行缩放
+                .aspectRatio(contentMode: .fit) // 设置图像的纵横比
+            HStack {
+                VStack(alignment: .leading) {
+                    Text(category)
+                        .font(.headline)
+                        .foregroundColor(.secondary)
+                    Text(heading)
+                        .font(.title)
+                        .fontWeight(.black)
+                        .foregroundColor(.primary)
+                        .lineLimit(3)
+                    Text("Written by \(author)".uppercased())
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                }
+                Spacer()
+            }
+            .padding()
+        }
+        .cornerRadius(10)
+        .overlay {
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(Color(.sRGB, red: 150/255, green: 150/255, blue: 150/255, opacity: 0.1), lineWidth: 1)
+        }
+        .padding([.top, .horizontal])
     }
 }
 
