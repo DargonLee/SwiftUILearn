886d92cf2bdb18b1e5d11fb6a416c896302db9cf
Harlans <2461414445@qq.com>
2024-07-28 16:40:55 +0800

    增加课程4、5

---
+++ SwiftUILearn/04Stack.swift
@@ -0,0 +1,18 @@
+//
+//  4Stack.swift
+//  SwiftUILearn
+//
+//  Created by Harlans on 2024/7/27.
+//
+
+import SwiftUI
+
+struct _Stack: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    _Stack()
+}
