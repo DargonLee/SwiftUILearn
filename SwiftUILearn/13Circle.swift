be869ddc27ccafa25f754cf541fbd127f0959250
Harlan <2461414445@qq.com>
2025-11-05 19:41:47 +0800

    Circle

---
+++ SwiftUILearn/13Circle.swift
@@ -0,0 +1,18 @@
+//
+//  13Circle.swift
+//  SwiftUILearn
+//
+//  Created by Harlan on 2025/11/5.
+//
+
+import SwiftUI
+
+struct _3Circle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    _3Circle()
+}
